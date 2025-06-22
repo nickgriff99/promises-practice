9da26752387d6416f91c1bc8209bedf9ab1d9845
Nicholas Griffiths <nichg999@gmail.com>
2025-06-21 21:51:21 -0600

    finished exercise 9

--- exercises/e9.js
+++ exercises/e9.js
@@ -13,7 +13,8 @@
 
 export function iterate(arg) {
   // Your code goes here...
-  
+  console.log(arg);
+  return arg + 1;
 }
 
 /**
@@ -24,7 +25,7 @@ export function iterate(arg) {
 
 export function alwaysThrows() {
   // Your code goes here...
-
+  throw new Error("OH NOES");
 }
 
 /**
@@ -36,9 +37,13 @@ export function alwaysThrows() {
  * The function must be exported
  */
 
-export function onReject() {
+export function onReject(arg) {
   // Your code goes here...
-
+  if (arg && typeof arg === 'object' && arg.message) {
+    console.log(arg.message);
+  } else {
+    console.log(arg);
+  }
 }
 
 /**
@@ -63,7 +68,18 @@ export function onReject() {
  */
 
 // Your code goes here...
-export const promise;
+export const promise = Promise.resolve(1)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(alwaysThrows)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .catch(onReject);
 
 
 
