771e9b87a86988e9d81a5430b6c8981f3580acda
Nicholas Griffiths <nichg999@gmail.com>
2025-06-22 22:48:55 -0600

    finshed exercise 11

--- exercises/e11.js
+++ exercises/e11.js
@@ -13,6 +13,7 @@
  * * set the usersUrl constant to store the json-server 'users' endpoint path
 */
 
+import fetch from 'node-fetch';
 export const usersUrl = 'http://localhost:3000/users/';
 
 /**
@@ -26,9 +27,9 @@ export const usersUrl = 'http://localhost:3000/users/';
  * Example: const getLoginList = (data) => {<Your code>}
 */
 
-const getLoginList = () => {
+const getLoginList = (data) => {
   // Your code goes here...
-
+  return data.map(user => user.login);
 }
 
 /**
@@ -39,7 +40,7 @@ const getLoginList = () => {
 */
 
 // Your code goes here ...
-const getData;
+const getData = fetch(usersUrl);
 
 /**
  * @task 
@@ -53,7 +54,13 @@ const getData;
 */
 
 // Your code goes here ...
-export const result = getData;
+export const result = getData
+  .then(response => response.json())
+  .then(data => {
+    const logins = getLoginList(data);
+    console.log(logins);
+    return logins;
+  });
 
 
 // === TEST YOURSELF ===
